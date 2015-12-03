8c0f9968710c894ce9898b44b65809f206bb969d
Igor Silva <igorjose15@gmail.com>
2015-12-03 11:13:19 -0200

    Finalizando projeto

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,17 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('callidus', ['ionic','callidus.controllers','btford.socket-io',])
+angular.module('callidus', ['ionic','ionic.service.core','callidus.controllers','btford.socket-io',])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+    });
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,32 +3,72 @@
  */
 angular.module('callidus.controllers',[]).
 
-  controller('callidus_controller',function($scope,mySocket){
+  controller('callidus_controller',function($scope,mySocket,$ionicPopup){
 
     mySocket.on('resposta',function(data){
-      console.log(data)
+
+      if(data.relay == "relay001"){
+        if (data.ligado){
+          $scope.lampadaActive = true;
+
+          if(data.dia){
+            $scope.showAlert();
+          }
+        }else{
+          $scope.lampadaActive = false;
+        }
+
+      }else if(data.relay == "relay002"){
+        if (data.ligado){
+          $scope.monitorActive = true;
+        }else{
+          $scope.monitorActive = false;
+        }
+      }
 
     });
     mySocket.on('status',function(data){
-      console.log(data);
+
+      for(key in data){
+        if(key == "relay001"){
+          if (data[key]){
+            $scope.lampadaActive = true;
+
+          }else{
+            $scope.lampadaActive = false;
+          }
+        }else if(key == "relay002"){
+          if (data[key]){
+            $scope.monitorActive = true;
+          }else{
+            $scope.monitorActive = false;
+          }
+        }
+      }
+
     });
 
+
     $scope.releControl = function (rele) {
-      var data = {rele: rele};
+      var data = {relay: rele};
 
       mySocket.emit('acionado',data)
     }
 
 
     $scope.getReleStatus = function () {
-
       mySocket.emit('getStatus')
     }
 
-    $scope.isActive = false;
-    $scope.activeButton = function() {
-      $scope.isActive = !$scope.isActive;
-    }  
+    $scope.showAlert = function() {
+      var alertPopup = $ionicPopup.alert({
+        title:'<span class="icon ion-leaf"></span>',
+        template: ' <h3><b>O dia está lindo! Você poderia estar economizando!  =/</b></h3>'
+      });
+
+    };
+
+
 
   });
 
@@ -38,5 +78,5 @@ classApp.controller('classCtrl', function ($scope) {
   $scope.isActive = false;
   $scope.activeButton = function() {
     $scope.isActive = !$scope.isActive;
-  }  
+  }
 });
